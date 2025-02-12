f489823d8227e966367bdd6ee6f651726e697f9a
keiakihito <kkatsumi.git@gmail.com>
2025-02-12 11:24:29 -0800

    Revise frontend diagram

--- backend/app/loaders/dataloader.py
+++ backend/app/loaders/dataloader.py
@@ -1,24 +1,3 @@
-# # dataloader.py
-# from app.dbmodels.models import Player, PlayerStats, Shot, Game
-#
-# class DataLoader:
-#     def get_player_data(self, playerID):
-#         # Query the database for player info
-#         return Player.objects.filter(player_id=playerID).values()  # Use `id` if `player_id` does not exist
-#
-#     def get_player_stats_data(self, playerID):
-#         # Query the database for player stats
-#         return PlayerStats.objects.filter(player_id=playerID).values()
-#
-#     def get_player_shots_in_a_game(self, playerStatsID):
-#         # Query the database for shots in a specific game for a single player
-#         return Shot.objects.filter(player_stats_id=playerStatsID).values()
-#
-#     def get_participated_game(self, gameID):
-#         # Query the database for game in which a single player participated
-#         return Game.objects.filter(game_id=gameID).values()
-
-
 # dataloader.py
 from app.dbmodels.models import Player, PlayerStats, Shot, Game
 
